933bc3ba79fae6b71e34cedbcadde80d3fe4258e
Team Drive <320Drive@gmail.com>
2014-12-02 00:20:53 -0500

    Added pagination

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ skrollr.init(
 	}
 );
 
+var query = '';
+var start = 0;
 $('#search-form').submit(function(e) {
     e.preventDefault();
     $('.search-section').fadeOut('normal', function() {
@@ -17,29 +19,35 @@ $('#search-form').submit(function(e) {
     $('.content-wrapper').css('padding', '150px 0px 400px');
     $('.common-queries').hide();
     $('.about').hide();
-    var query = $('#search-field').val();
-    query = parseQuery(query);
-    solrSearch(query);
+    query = $('#search-field').val();
+    query = parseQuery();
+    solrSearch();
     $('.search-results').show();
+	$('.search-results .container').on('click', '#next-page', function() {
+		solrSearch();
+	});
 });
 
-function parseQuery(query) {
+function parseQuery() {
     var parsedQuery = query.split(' ').join('+');
     return parsedQuery;
 }
 
-function solrSearch(query) {
+function solrSearch() {
+	start = start || 0;
     $.ajax({
         async: false,
         type: 'GET',
-        url: 'https://128.208.102.114:8443/solr-example/collection1/select',
+        url: 'http://128.208.102.114:8080/select',
         dataType: 'JSONP',
         data: {
             'q': 'content:' + query,
             'wt': 'json',
             'indent': false,
             'fl': 'url,title',
-            'score': 'score desc'
+            'score': 'score desc',
+			'rows': '10',
+			'start': start
         },
         jsonp: 'json.wrf',
         success: function (data, textStatus, jqXHR) {
@@ -49,10 +57,14 @@ function solrSearch(query) {
 }
 
 function renderSearchResults(searchResults) {
+	$('.search-results .container').empty();
+	start = searchResults.start + 10;
     for (var i = 0; i < searchResults.docs.length; i++) {
         var title = searchResults.docs[i].title;
         var url = searchResults.docs[i].url;
         var resultItem = $('<a>', {href: url, text: title});
         $('.search-results .container').append(resultItem).append('<br />');
     }
+	var nextLink = $('<a>', {href: '#', text: 'Next page', id: 'next-page'});
+	$('.search-results .container').append(nextLink);
 }
