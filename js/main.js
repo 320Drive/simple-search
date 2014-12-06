a8cece843d8b370d29204c69150be60127654dce
Shijir Tsogoo <shijir@freshconsulting.com>
2014-12-05 18:15:06 -0800

    optimized with angular

--- js/main.js
+++ js/main.js
@@ -1,14 +1,52 @@
-skrollr.init(
-	{
-		smoothScrolling:true,
-		smoothScrollingDuration:2000,
-        scale:1
-		// render: function(data) {
-	 //        Log the current scroll position.
-	 //        console.log(data.curTop);
-	 //    }
-	}
-);
+skrollr.init({
+    smoothScrolling: true,
+    smoothScrollingDuration: 2000,
+    scale: 1
+        // render: function(data) {
+        //        Log the current scroll position.
+        //        console.log(data.curTop);
+        //    }
+});
+
+/*==========  Angular Data | Start ==========*/
+angular.module('driveApp', [])
+    .controller('display', function($scope) {
+
+        $scope.queries = [{
+            "query": "car oil change"
+        }, {
+            "query": "filter change"
+        }, {
+            "query": "maintenance"
+        }, {
+            "query": "car noise"
+        }, {
+            "query": "reduce noise"
+        }, {
+            "query": "used cars"
+        }, {
+            "query": "BMW 4 series"
+        }, {
+            "query": "safe driving"
+        }, {
+            "query": "reduce smoke"
+        }, {
+            "query": "used tires"
+        }, {
+            "query": "honda accord"
+        }, {
+            "query": "electic cars"
+        }];
+
+        $scope.searchQuery = function(query){
+            $scope.searchInput=query;
+        }
+
+
+    });
+/*==========  Angular Data | End ==========*/
+
+
 
 var query = '';
 var start = 0;
@@ -16,8 +54,8 @@ $('#search-form').submit(function(e) {
     e.preventDefault();
 
     $.smoothScroll({
-      scrollTarget: '.content-wrapper',
-      offset: -100
+        scrollTarget: '.content-wrapper',
+        offset: -100
     });
 
     $('.common-queries').hide();
@@ -25,18 +63,18 @@ $('#search-form').submit(function(e) {
     query = $('#search-field').val();
     query = parseQuery();
 
-    $('.no-results strong').html(' '+query);
+    $('.no-results strong').html(' ' + query);
 
     solrSearch();
     $('.search-results').show();
-	$('.search-results .container').on('click', '#prev-page', function() {
-		start = start - 10;
-		solrSearch();
-	});
-	$('.search-results .container').on('click', '#next-page', function() {
-		start = start + 10;
-		solrSearch();
-	});
+    $('.search-results .container').on('click', '#prev-page', function() {
+        start = start - 10;
+        solrSearch();
+    });
+    $('.search-results .container').on('click', '#next-page', function() {
+        start = start + 10;
+        solrSearch();
+    });
 });
 
 function parseQuery() {
@@ -56,11 +94,11 @@ function solrSearch() {
             'indent': false,
             'fl': 'url,title',
             'score': 'score desc',
-			'rows': '10',
-			'start': start
+            'rows': '10',
+            'start': start
         },
         jsonp: 'json.wrf',
-        success: function (data, textStatus, jqXHR) {
+        success: function(data, textStatus, jqXHR) {
             renderSearchResults(data.response);
             console.log(data.response);
         }
@@ -71,27 +109,40 @@ function renderSearchResults(searchResults) {
     $('a.list-group-item').remove();
     $('.no-results').hide();
 
-    $('#result-stat').fadeIn(1000).html(searchResults.numFound+" results.");
-    if(searchResults.numFound===0){
+    $('#result-stat').fadeIn(1000).html(searchResults.numFound + " results.");
+    if (searchResults.numFound === 0) {
         $('.no-results').fadeIn(1000);
     }
     for (var i = 0; i < searchResults.docs.length; i++) {
         var title = searchResults.docs[i].title;
         var url = searchResults.docs[i].url;
-        var resultItem = $('<a>', {href: url, target:'_blank', class:'list-group-item', text: title});
+        var resultItem = $('<a>', {
+            href: url,
+            target: '_blank',
+            class: 'list-group-item',
+            text: title
+        });
         $('.search-results .list-group').append(resultItem);
     }
-	$('.search-results').append('<br />');
-	var prevLink = $('<a>', {href: '#', text: 'Prev page', id: 'prev-page'});
-	var nextLink = $('<a>', {href: '#', text: 'Next page', id: 'next-page'});
-	if (start > 0) {
-		$('.search-results .container').append(prevLink);
-	}
-	if ((start + 10) < searchResults.numFound) {
-		$('.search-results .container').append(nextLink);
-	}
+    $('.search-results').append('<br />');
+    var prevLink = $('<a>', {
+        href: '#',
+        text: 'Prev page',
+        id: 'prev-page'
+    });
+    var nextLink = $('<a>', {
+        href: '#',
+        text: 'Next page',
+        id: 'next-page'
+    });
+    if (start > 0) {
+        $('.search-results .container').append(prevLink);
+    }
+    if ((start + 10) < searchResults.numFound) {
+        $('.search-results .container').append(nextLink);
+    }
 }
-$('document').ready(function(){
+$('document').ready(function() {
 
     $('#result-stat').hide();
     $('.no-results').hide();
