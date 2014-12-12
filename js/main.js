4ff4b4b350b0ae74a5fbb1171c7196d8fce93882
Shijir Tsogoo <shijir@freshconsulting.com>
2014-12-11 20:45:28 -0800

    pagination added

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ skrollr.init({
         //    }
 });
 
+var resultsJSON;
+
 /*==========  Angular Data | Start ==========*/
 angular.module('driveApp', [])
     .controller('display', function($scope) {
@@ -59,11 +61,6 @@ var start = 0;
 $('#search-form').submit(function(e) {
     e.preventDefault();
 
-    $.smoothScroll({
-        scrollTarget: '.content-wrapper',
-        offset: -100
-    });
-
     $('.common-queries').hide();
     $('.about').hide();
     $('.articles').hide();
@@ -74,18 +71,18 @@ $('#search-form').submit(function(e) {
 
     solrSearch();
     $('.search-results').show();
-    $('.search-results .container').on('click', '#prev-page', function() {
+    $('.search-results .pagination').on('click', '#prev-page', function() {
         start = start - 10;
         solrSearch();
     });
-    $('.search-results .container').on('click', '#next-page', function() {
+    $('.search-results .pagination').on('click', '#next-page', function() {
         start = start + 10;
         solrSearch();
     });
 });
 
 function parseQuery() {
-    var parsedQuery = query.split(' ').join('+');
+    var parsedQuery = query.split(' ').join(' ');
     return parsedQuery;
 }
 
@@ -107,12 +104,23 @@ function solrSearch() {
         jsonp: 'json.wrf',
         success: function(data, textStatus, jqXHR) {
             renderSearchResults(data.response);
-            console.log(data.response);
+            resultsJSON=data.response;
+            console.log(resultsJSON);  
         }
     });
 }
 
 function renderSearchResults(searchResults) {
+
+    var resultNumbers = searchResults.numFound;
+    var startID = searchResults.start; 
+
+    $.smoothScroll({
+        scrollTarget: '.content-wrapper',
+        offset: -100,
+        speed:500
+    });
+
     $('a.list-group-item').remove();
     $('.no-results').hide();
 
@@ -133,21 +141,15 @@ function renderSearchResults(searchResults) {
         $('.search-results .list-group').append(resultItem);
     }
     $('.search-results').append('<br />');
-    var prevLink = $('<a>', {
-        href: '#',
-        text: 'Prev page',
-        id: 'prev-page'
-    });
-    var nextLink = $('<a>', {
-        href: '#',
-        text: 'Next page',
-        id: 'next-page'
-    });
+    $('.search-results .pagination li').remove();
+    var prevLink = $('<li id="prev-page"><a href="#"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>');
+    var nextLink = $('<li id="next-page"><a href="#"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>');
     if (start > 0) {
-        $('.search-results .container').append(prevLink);
+        $('.search-results .pagination').append(prevLink);
     }
+
     if ((start + 10) < searchResults.numFound) {
-        $('.search-results .container').append(nextLink);
+        $('.search-results .pagination').append(nextLink);
     }
 }
 $('document').ready(function() {
@@ -156,3 +158,4 @@ $('document').ready(function() {
     $('.no-results').hide();
 
 });
+
